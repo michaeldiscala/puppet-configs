669d208e4bf9c4ee9be4edb5f150f2021ff8f640
Michael DiScala <mike.discala@gmail.com>
2012-11-18 20:27:38 +0000

    rvm working; passenger in progress

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,9 +1,18 @@
 node default {
+	$ruby_version = 'ruby-1.9.3-p327'
+	$passenger_version = '3.0.17'
+
 	include base
+	include httpd
 	include rvm
-	rvm_system_ruby { 'ruby-1.9.3-p327':
+	rvm_system_ruby { $ruby_version:
 		ensure      => "present",
 		default_use => true,
 	}
+	class { "rvm::passenger::apache":
+		ruby_version => $ruby_version,
+		version      => $passenger_version,
+		require      => Class['httpd','rvm'],
+	} 
 
 }
